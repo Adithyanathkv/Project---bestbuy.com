cece58e0d35d28bea81b5359268600005f042bbe
Adithyanathkv <adithyanathmite@gmail.com>
2022-02-25 12:16:48 +0530

    cartpage,paymentpage and thanksforpurchasing page

---
+++ bestbuy/navbar.js
@@ -0,0 +1,10 @@
+
+function goto(){
+    var searchtextvalue=document.querySelector("#search").value
+    console.log(searchtextvalue)
+    console.log("hello")
+    if(searchtextvalue=="tv"){
+        window.location.href="signup.html"
+    }
+    
+}
